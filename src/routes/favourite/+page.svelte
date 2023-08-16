4a16dd82d8cc7b9aaccf3f15cbb036e3915db7bc
highscore <highscoretechng@gmail.com>
2023-08-16 11:54:13 +0100

    thried

--- src/routes/favourite/+page.svelte
+++ src/routes/favourite/+page.svelte
@@ -1,3 +1,6 @@
-<div class="Favourite">
-    <h1>Favourite Page</h1>
+<div class="sc-lhMiDA ePAxUv">
+    <div class="sc-eCImPb cuPxwd empty full-abs">
+        <img alt="" src="https://static.nanogames.io/assets/empty.acd1f5fe.png">
+        <div class="msg">Sorry, please log in first! </div>
+    </div>
 </div>
--- src/routes/recent-play/+page.svelte
+++ src/routes/recent-play/+page.svelte
@@ -1,5 +1,6 @@
-<div class="recent-play">
-
+<div class="sc-lhMiDA ePAxUv">
+    <div class="sc-eCImPb cuPxwd empty full-abs">
+        <img src="https://static.nanogames.io/assets/empty.acd1f5fe.png" alt="">
+        <div class="msg">Sorry, please log in first! </div>
+    </div>
 </div>
-
-
