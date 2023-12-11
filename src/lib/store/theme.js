79df02877a9807c024b84ec6392c6d3f309a96a6
John Idoko <idokojohn42@yahoo.com>
2023-12-11 05:45:57 +0100

    Store Theme Mode in Local Storage

--- src/lib/store/theme.js
+++ src/lib/store/theme.js
@@ -1,3 +1,20 @@
 import { writable } from 'svelte/store';
 
-export const isLightMode = writable(false);
+let initialIsLightMode = false;
+
+// Check if running in a browser environment
+if (typeof window !== 'undefined') {
+  // Check if the value is stored in local storage
+  const storedValue = localStorage.getItem('isLightMode');
+  initialIsLightMode = storedValue ? JSON.parse(storedValue) : false;
+}
+
+// Create a writable store with the initial value
+export const isLightMode = writable(initialIsLightMode);
+
+// Subscribe to changes and update local storage
+isLightMode.subscribe(value => {
+  if (typeof window !== 'undefined') {
+    localStorage.setItem('isLightMode', JSON.stringify(value));
+  }
+});
