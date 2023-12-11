15956b9621139debf9715f73dba28b2b45c535f0
highscoretech <highscoretechng@gmail.com>
2023-12-11 13:21:44 +0100

    mines

--- src/routes/(games)/crash/+page.svelte
+++ src/routes/(games)/crash/+page.svelte
@@ -47,7 +47,7 @@ import Allbet from "./allbet.svelte";
 
 
 </script>
-<h3>Hello bro667897</h3>
+<h3>Hello bro090lkn7</h3>
 <!-- {#if !is_loading}
     <div style={`${$is_open__chat && $is_open__Appp && $screen > 1579 || $is_open__chat && !$is_open__Appp && $screen > 1219 || !$is_open__chat && !$is_open__Appp && $screen > 1049 || !$is_open__chat && $is_open__Appp && $screen > 1214 ? "" : "display:none"}`} id="crash-main">
         <div class="sc-lhMiDA ePAxUv" style="opacity: 1; transform: none;">
