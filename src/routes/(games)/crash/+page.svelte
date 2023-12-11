3441169fa295568a20909e8a51af7c7f08753545
highscoretech <highscoretechng@gmail.com>
2023-12-11 13:25:49 +0100

    mines

--- src/routes/(games)/crash/+page.svelte
+++ src/routes/(games)/crash/+page.svelte
@@ -47,7 +47,7 @@ import { crash_historyEl} from "$lib/crashgame/store"
 
 
 </script>
-<h3>Hello brobbkn7</h3>
+<h3>Hello bropppbbkn7</h3>
 <!-- {#if !is_loading}
     <div style={`${$is_open__chat && $is_open__Appp && $screen > 1579 || $is_open__chat && !$is_open__Appp && $screen > 1219 || !$is_open__chat && !$is_open__Appp && $screen > 1049 || !$is_open__chat && $is_open__Appp && $screen > 1214 ? "" : "display:none"}`} id="crash-main">
         <div class="sc-lhMiDA ePAxUv" style="opacity: 1; transform: none;">
