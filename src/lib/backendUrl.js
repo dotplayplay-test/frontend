dcae95ee10cb6976ce5e048301108311bc412732
highscoretech <highscoretechng@gmail.com>
2023-12-11 12:52:41 +0100

    mines

--- src/lib/backendUrl.js
+++ src/lib/backendUrl.js
@@ -1,9 +1,9 @@
 export const ServerURl = (()=>{ 
-    // return "http://localhost:8000"
-    return "https://dotplayplay-3a34f3d133a0.herokuapp.com"
+    return "http://localhost:8000"
+    // return "https://dotplayplay-3a34f3d133a0.herokuapp.com"
 })
 
 export const RealTimeURl = (()=>{ 
-    // return "http://localhost:3000"
-    return "https://dpp-f82256e6e6d2.herokuapp.com"
+    return "http://localhost:3000"
+    // return "https://dpp-f82256e6e6d2.herokuapp.com"
 })
