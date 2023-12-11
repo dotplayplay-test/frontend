e6053bd641b1a440db0d88afd8d48c08e14f3600
highscoretech <highscoretechng@gmail.com>
2023-12-11 16:46:40 +0100

    clean

--- src/routes/(games)/crash/+page.svelte
+++ src/routes/(games)/crash/+page.svelte
@@ -25,20 +25,20 @@ const handleNavigation = ((w) => {
 })
 
 let is_loading = false
-// const handleCrashHistory = (async()=>{
-//     await axios.post(`${URL}/api/user/crash-game/history`)
-//     .then((result) => {
-//         is_loading = false
-//         crash_historyEl.set(result.data)
-//     }).catch((err) => {
-//         is_loading = false
-//         console.log(err)  
-//     });
-// })
+const handleCrashHistory = (async()=>{
+    await axios.post(`${URL}/api/user/crash-game/history`)
+    .then((result) => {
+        is_loading = false
+        crash_historyEl.set(result.data)
+    }).catch((err) => {
+        is_loading = false
+        console.log(err)  
+    });
+})
 
-// onMount(async()=>{
-//   await handleCrashHistory()
-// })
+onMount(async()=>{
+  await handleCrashHistory()
+})
    
 
 
