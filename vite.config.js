f540a2393785a0f53510ca1248424395e1b7f7c7
CDiala <dialachibuzo@yahoo.com>
2024-01-11 21:48:26 +0100

    feat: setting up socket integration, in progress

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,31 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+
+import { Server } from "socket.io";
+
+const webSocketServer = {
+  name: "webSocketServer",
+  configureServer(server) {
+    if (!server.httpServer) return;
+
+    const io = new Server(server.httpServer);
+
+    io.on("connection", (data) => {
+      socket.emit("latest-bet", "hello world!");
+
+      socket.on("active-bets-keno", (content, id) => {
+        try {
+          //   console.log("content:", content);
+          socket.emit("latest-bet", content);
+        } catch (error) {
+          //   console.log("error:", error);
+          socket.emit("latest-bet", error);
+        }
+      });
+    });
+  },
+};
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit(), webSocketServer],
 });
