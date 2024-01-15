1b328bc50ac17d018e8afef21fe09f286638fb24
Adelodun Damilare <adelodundamilare@gmail.com>
2024-01-15 21:59:06 +0100

    ramping up; adding info text

---
+++ src/lib/achieve/AchieveSingle.svelte
@@ -0,0 +1,101 @@
+<script>
+  import { goto } from "$app/navigation";
+  import Dialog from "./Dialog.svelte";
+
+  const handleCancel = () => {
+    goto("/promotion");
+  };
+
+  export let selectedMedal;
+</script>
+
+<Dialog title="Medals Details" on:cancel={handleCancel}>
+  {#if selectedMedal}
+    <div class="head">
+      <img src="/achieve/{selectedMedal.src}" alt={selectedMedal.name} />
+      <p class="title">{selectedMedal.name}</p>
+      <div class="progress"></div>
+      <p>0/200</p>
+    </div>
+
+    <div class="info">
+      {selectedMedal?.info}
+    </div>
+
+    <div class="metric"><span>Quantity</span><span>Unlimited</span></div>
+    <div class="metric"><span>Duration</span><span>Unlimited</span></div>
+
+    <div class="footer">
+      <svg
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        class="sc-gsDKAQ hxODWG icon"
+        ><use xlink:href="#icon_Firends"></use></svg
+      >
+      0
+    </div>
+  {/if}
+</Dialog>
+
+<style>
+  .head {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .title {
+    font-size: 1.125rem;
+    margin: 0.9375rem;
+  }
+  .progress {
+    width: 200px;
+    height: 6px;
+    margin: 10px 0;
+    background: var(--affiliate-bg);
+  }
+  .info {
+    border: 1px solid rgb(67, 179, 9);
+    color: rgb(67, 179, 9);
+    padding: 20px;
+    border-radius: 20px;
+    margin-top: 50px;
+  }
+  .metric {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 10px;
+    background-color: rgba(45, 48, 53, 0.5);
+    padding: 1.25rem;
+    border-radius: 1.25rem;
+  }
+  .metric span:nth-child(1) {
+    font-weight: 600;
+    color: rgba(153, 164, 176, 0.6);
+  }
+  .footer {
+    border-radius: 1.375rem;
+    padding: 0.625rem;
+    display: flex;
+    border: 1px solid rgba(153, 164, 176, 0.13);
+    align-items: center;
+    height: 3.5rem;
+    justify-content: center;
+    font-size: 0.75rem;
+    margin-top: 3.5rem;
+    font-weight: 600;
+    width: 100%;
+  }
+  svg {
+    width: 1rem;
+    height: 1rem;
+    fill: rgb(67, 179, 9);
+    margin-right: 0.3125rem;
+  }
+
+  @media screen and (max-width: 650px) {
+    *,
+    *:before,
+    *:after {
+      box-sizing: border-box;
+    }
+  }
+</style>
--- src/routes/achieve/+page.svelte
+++ src/routes/achieve/+page.svelte
@@ -1,29 +1,34 @@
 <script>
   import { goto } from "$app/navigation";
-  import { browser } from "$app/environment";
-  import { onMount } from "svelte";
-  import Card from "../promotion/Card.svelte";
-  import Info from "../promotion/Info.svelte";
-  import Medals from "../promotion/Medals.svelte";
-  import Dialog from "../../lib/achieve/Dialog.svelte";
+  import Dialog from "$lib/achieve/Dialog.svelte";
+  import AchieveList from "$lib/achieve/AchieveList.svelte";
+  import AchieveSingle from "../../lib/achieve/AchieveSingle.svelte";
 
   const handleCancel = () => {
     goto("/promotion");
   };
 
-  onMount(() => {
-    if (browser && window.innerWidth < 650) {
-      is_mobile = true;
-    } else {
-      is_mobile = false;
-    }
-  });
-
   const medals = [
-    { src: "achieve_1.webp", name: "Talkative" },
-    { src: "achieve_2.webp", name: "Fearless One" },
-    { src: "achieve_3.png", name: "The Loaded King" },
-    { src: "achieve_4.webp", name: "Highest Contributor" },
+    {
+      src: "achieve_1.webp",
+      name: "Talkative",
+      info: "Win this precious medal by chatting in any room for a total of 200days. (Does not need to contribute 200 days)",
+    },
+    {
+      src: "achieve_2.webp",
+      name: "Fearless One",
+      info: "No one haas lost more than you, you have experienced the worst things! You are a tough cookie! Nothing can knock you down the fearless!",
+    },
+    {
+      src: "achieve_3.png",
+      name: "The Loaded King",
+      info: "You are rolling in money! Win this bling medal if you have the highest wallet balance ever in the history of DOTPLAYPLAY.IO! (P.S. Vault balance excluded)",
+    },
+    {
+      src: "achieve_4.webp",
+      name: "Highest Contributor",
+      info: "You are a true king! The mighty heart! The one with the highest wager ever in the history of DOTPLAYPLAY.IO!",
+    },
     { src: "achieve_5.webp", name: "The Top Gun" },
     { src: "achieve_6.png", name: "The Rain Master" },
     { src: "achieve_7.webp", name: "PACO Lover" },
@@ -41,75 +46,18 @@
     { src: "achieve_17.webp", name: "NANOTOP 1" },
     { src: "achieve_18.webp", name: "BTCTOP 1" },
   ];
+
+  $: selectedMedal = null;
+
+  const selectMedal = (e) => {
+    selectedMedal = e.detail;
+  };
 </script>
 
 <Dialog title="Master Medals" on:cancel={handleCancel}>
-  <div>
-    <div class="medals">
-      <Medals />
-    </div>
-    <Info />
-    <div class="divider"></div>
-    <div class="">
-      <div class="card-box">
-        {#each medals as medal}
-          <Card {medal} />
-        {/each}
-      </div>
-
-      <div class="footer">
-        <svg
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          class="sc-gsDKAQ hxODWG icon"
-          ><use xlink:href="#icon_Inform"></use></svg
-        >
-        Tip CUB is not included in medal calculation Stats Updated every 24 hours
-      </div>
-    </div>
-  </div>
+  {#if selectedMedal}
+    <AchieveSingle {selectedMedal} medal={medals[selectedMedal]} />
+  {:else}
+    <AchieveList {medals} on:selectMedal={selectMedal} />
+  {/if}
 </Dialog>
-
-<style>
-  .divider {
-    width: 100%;
-    height: 2px;
-    background-color: rgb(34, 36, 40);
-    margin: 20px 0;
-  }
-
-  .medals {
-    margin: 20px 0;
-  }
-
-  .icon {
-    width: 1rem;
-    height: 1rem;
-    margin-right: 0.25rem;
-    display: inline-block;
-    vertical-align: text-top;
-    fill: rgb(67, 179, 9);
-  }
-
-  .footer {
-    text-align: center;
-    font-weight: 600;
-    font-size: 0.75rem;
-    color: rgb(67, 179, 9);
-    margin: 0 auto;
-    margin-top: 2rem;
-    max-width: 300px;
-  }
-  .card-box {
-    display: grid;
-    grid-gap: 6px;
-    grid-template-columns: 1fr 1fr 1fr;
-  }
-
-  @media screen and (max-width: 650px) {
-    *,
-    *:before,
-    *:after {
-      box-sizing: border-box;
-    }
-  }
-</style>
